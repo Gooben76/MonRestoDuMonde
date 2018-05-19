84a154f74ea2b350261d2cfc4be3b7baeabd195c
Benoît Goossens <benoitgoossens@macben.lan>
2018-05-19 15:55:38 +0200

    Ajout des images dans xassets

---
+++ MonRestoDuMonde/Controlleur/MenuController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by Benoît Goossens on 19/05/18.
+//  Copyright © 2018 Benoît Goossens. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
