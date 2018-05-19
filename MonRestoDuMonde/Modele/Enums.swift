175c648ed946a2698ca7fef8637227b5d82eae96
Benoît Goossens <benoitgoossens@macben.lan>
2018-05-19 20:33:32 +0200

    Ajout de la classe Plat

---
+++ MonRestoDuMonde/Modele/Enums.swift
@@ -0,0 +1,15 @@
+//
+//  Enums.swift
+//  MonRestoDuMonde
+//
+//  Created by Benoît Goossens on 19/05/18.
+//  Copyright © 2018 Benoît Goossens. All rights reserved.
+//
+
+import Foundation
+
+enum Type{
+    case entree
+    case platPrincipal
+    case dessert
+}
---
+++ MonRestoDuMonde/Modele/Plat.swift
@@ -0,0 +1,36 @@
+//
+//  Plat.swift
+//  MonRestoDuMonde
+//
+//  Created by Benoît Goossens on 19/05/18.
+//  Copyright © 2018 Benoît Goossens. All rights reserved.
+//
+
+import UIKit
+
+class Plat{
+    private var _type:Type
+    private var _pays:String
+    private var _desc:String
+    private var _image:UIImage
+    
+    var type:Type{
+        return _type
+    }
+    var pays:String{
+        return _pays
+    }
+    var desc:string{
+        return _desc
+    }
+    var image:UIImage{
+        return _image
+    }
+    
+    init(type:Type, pays:String, desc:String, image:UIImage) {
+        self._type = type
+        self._pays = pays
+        self._desc = desc
+        self._image = image
+    }
+}
