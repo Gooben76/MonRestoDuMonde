193fcf05c01d892148118793e08ba8f56019c37c
Benoît Goossens <benoitgoossens@macben.lan>
2018-05-19 20:22:53 +0200

    Ajout du UICollectionView

---
+++ MonRestoDuMonde/Vue/CellCollection.swift
@@ -0,0 +1,13 @@
+//
+//  CellCollection.swift
+//  MonRestoDuMonde
+//
+//  Created by Benoît Goossens on 19/05/18.
+//  Copyright © 2018 Benoît Goossens. All rights reserved.
+//
+
+import UIKit
+
+class CellCollection: UICollectionViewCell {
+    
+}
